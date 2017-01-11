468567651a51423747150e9c6c80dbdf322e295b
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-11 18:04:57 -0500

    Get Stream data with hard coded channel name

--- js/twitchview.js
+++ js/twitchview.js
@@ -1,5 +1,8 @@
-function searchTwitch() {
-
+function getStream() {
+	var channelName = 'gamedonequick'; //example
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + channelName + 'gamesdonequick?callback=?', data => {
+	    console.log(data.stream.game);
+	})
 }
 
 function refreshResults() {
