fa0a091b23279efe05bad312193633115140b9b7
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-11 21:35:18 -0500

    Get data for channels that are Online

--- js/twitchview.js
+++ js/twitchview.js
@@ -1,8 +1,34 @@
 function getStream() {
-	var channelName = 'gamedonequick'; //example
-	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + channelName + 'gamesdonequick?callback=?', data => {
-	    console.log(data.stream.game);
-	})
+	var channels = ["freecodecamp", "gamesdonequick","storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
+	//var channelName = 'gamesdonequick'; //example
+	for(var i=0; i < channels.length; i++){
+		var channel = channels[i];
+		var url = 'https://wind-bow.gomix.me/twitch-api/streams/' + channel;
+
+		var channelData = {
+			channel: channels[i]
+		};
+
+		$.getJSON(url, channel, function(channelData, status) {
+			if(status === 'success'){
+				if(channelData.stream !== null){
+					console.log("CHANNEL ONLINE!!!");
+					var channelName = channelData.stream.channel.name;
+					var currentGame = channelData.stream.game;
+					var channelUrl = 'https://www.twitch.tv/'+channelName;
+					console.log("CHANNEL: " + channelName);
+					console.log("CURRENTLY PLAYING: " + currentGame);
+					console.log("WATCH AT: " + channelUrl);
+				}
+				else {
+					console.log("CHANNEL NOT ONLINE!");
+					console.log(channelData._links.channel);
+					console.log(channelData);
+				}
+			}
+		})
+
+	}
 }
 
 function refreshResults() {
