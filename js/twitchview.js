90325a22bb89cd0c87362c3569cd38b949f8749c
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-12 19:23:08 -0500

    Separate return object as either online, offline or dne

--- js/twitchview.js
+++ js/twitchview.js
@@ -1,36 +1,62 @@
-function getStream() {
-	var channels = ["freecodecamp", "gamesdonequick","storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
-	//var channelName = 'gamesdonequick'; //example
-	for(var i=0; i < channels.length; i++){
-		var channel = channels[i];
-		var url = 'https://wind-bow.gomix.me/twitch-api/streams/' + channel;
+var onlineChannels = [];
+var offlineChannels = [];
+var dneChannels = [];
 
-		var channelData = {
-			channel: channels[i]
-		};
+function getStream(channel) {
 
-		$.getJSON(url, channel, function(channelData, status) {
+	var url = 'https://wind-bow.gomix.me/twitch-api/streams/' + channel;
+
+	$.getJSON(url, function(channelData, status) {
+		if(status === 'success'){
+			if(channelData.stream !== null){
+				console.log("CHANNEL ONLINE!!!");
+				var channelName = channelData.stream.channel.name;
+				var currentGame = channelData.stream.game;
+				var channelUrl = 'https://www.twitch.tv/'+channelName;
+				console.log("CHANNEL: " + channelName);
+				console.log("CURRENTLY PLAYING: " + currentGame);
+				console.log("WATCH AT: " + channelUrl);
+				onlineChannels.push(channelData);
+			}
+			else {
+				console.log("CHANNEL NOT ONLINE!");
+				console.log(channel);
+				getChannel(channel);
+			}
+		}
+	})
+}
+
+function getChannel(channelName, offlineArray, dneArray) {
+	var url = 'https://wind-bow.gomix.me/twitch-api/channels/' + channelName;
+
+	$.getJSON(url, function(channelData, status) {
 			if(status === 'success'){
-				if(channelData.stream !== null){
-					console.log("CHANNEL ONLINE!!!");
-					var channelName = channelData.stream.channel.name;
-					var currentGame = channelData.stream.game;
-					var channelUrl = 'https://www.twitch.tv/'+channelName;
-					console.log("CHANNEL: " + channelName);
-					console.log("CURRENTLY PLAYING: " + currentGame);
-					console.log("WATCH AT: " + channelUrl);
+				if(channelData.status == 404) {
+					dneChannels.push(channelData);
 				}
-				else {
-					console.log("CHANNEL NOT ONLINE!");
-					console.log(channelData._links.channel);
-					console.log(channelData);
+				else{
+					offlineChannels.push(channelData);
 				}
 			}
 		})
-
-	}
 }
 
 function refreshResults() {
-	alert("refresh!");
+	//clear channel arrays
+	onlineChannels = [];
+	offlineChannels = [];
+	dneChannels = [];
+
+	var channels = ["freecodecamp", "gamesdonequick","storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
+	
+	for(var i=0; i<channels.length; i++) {
+		getStream(channels[i]);
+	}
 }
+
+ $(document).ajaxStop(function () {
+      console.log(onlineChannels);
+      console.log(offlineChannels);
+      console.log(dneChannels);
+  });
